51d8148a2b6b12438abdc98d747e0ca3ae343d80
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-07-09 14:57:10 -0500

    vueapp

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+let laSensor = new LinearAccelerationSensor({frequency: 60});
+
+laSensor.addEventListener("reading", (e) => {
+  console.log(`Linear acceleration along the X-axis ${laSensor.x}`);
+  console.log(`Linear acceleration along the Y-axis ${laSensor.y}`);
+  console.log(`Linear acceleration along the Z-axis ${laSensor.z}`);
+});
+laSensor.start();
+
+</script>
+
+<template>
+  <header>
+    <p>You did it</p>
+  </header>
+
+  <main>
+    <p>Hello</p>
+  </main>
+</template>
+
+<style scoped>
+</style>
