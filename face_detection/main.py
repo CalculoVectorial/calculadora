2c9fe3fb693cc4ab372604b31d05232415042c7f
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-08-11 18:14:23 -0500

    using opencv but just 2d, 3d too hard, future improvement

---
+++ face_detection/main.py
@@ -0,0 +1,42 @@
+import cv2
+
+# Load the pre-trained face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Initialize camera
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Convert to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        # Calculate face's position relative to the center
+        center_x = x + w // 2 - frame.shape[1] // 2
+        center_y = y + h // 2 - frame.shape[0] // 2
+        
+        print("X:", center_x, "Y:", center_y)
+
+        # Draw rectangle around the face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Face Tracking', frame)
+
+    # Check for user input to quit
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+# Release the camera and close all windows
+cap.release()
+cv2.destroyAllWindows()
